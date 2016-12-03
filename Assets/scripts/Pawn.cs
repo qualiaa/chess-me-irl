019b2c61280d2750566831928a2269791ce39168
Jamie Bayne <jamiebayne@hotmail.com>
2016-12-03 19:07:06 +0000

    Initial commit

---
+++ Assets/scripts/Pawn.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (Renderer))]
+[RequireComponent (typeof (Piece))]
+public class Pawn : MonoBehaviour {
+
+
+	private Vector3 lastMousePos_;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {}
+
+	void OnMouseDrag() {
+		var newCameraPos =  Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0));
+		var delta = newCameraPos - lastMousePos_;
+		transform.Translate (delta);
+		lastMousePos_ =  newCameraPos;
+	}
+
+	void OnMouseDown() {
+		var r = GetComponent<Renderer> ();
+		r.material.color = Color.cyan;
+		lastMousePos_ = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0));
+	}
+	
+	void OnMouseUp()
+	{
+		var r = GetComponent<Renderer> ();
+		var p = GetComponent<Piece> ();
+
+		r.material.color = p.pieceColor == PieceColor.White ? Color.white : Color.black;
+	}
+}
---
+++ Assets/scripts/Piece.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum PieceColor { Black, White };
+
+[RequireComponent (typeof (Renderer))]
+public class Piece : MonoBehaviour {
+
+
+	public PieceColor pieceColor = PieceColor.White;
+
+	// Use this for initialization
+	void Start () {
+		if (pieceColor == PieceColor.Black)
+		{
+			Renderer r = GetComponent<Renderer> ();
+			r.material.color = Color.black;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
