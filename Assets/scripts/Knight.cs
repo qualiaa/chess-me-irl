7b8f24e506a13ad3a9b13c1c0c936a6255dc7310
Jamie Bayne <jamiebayne@hotmail.com>
2016-12-04 11:59:01 +0000

    Add first Knight behaviour

---
+++ Assets/scripts/Knight.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (Rigidbody2D))]
+public class Knight : MonoBehaviour {
+
+	Rigidbody2D body_;
+	Vector3 lastMousePos_;
+
+	void Start () {
+		body_ = GetComponent<Rigidbody2D> ();
+	}
+		
+	void OnMouseDown() {
+		lastMousePos_ = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+	}
+
+	void OnMouseUp() {
+		var mousePos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+		var delta = mousePos - lastMousePos_;
+
+		body_.AddForce (new Vector2(-delta.x, -delta.y) * 100);
+	}
+}
